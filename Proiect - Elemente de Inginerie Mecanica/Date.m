c9d5432a3b389ed417e2d1e265104d053edaf249
Molnar T. Zsolt <126553055+Molnar-Zsolt-30123@users.noreply.github.com>
2024-03-08 13:51:43 +0200

    Add files via upload

---
+++ Proiect - Elemente de Inginerie Mecanica/Date.m
@@ -0,0 +1,37 @@
+%% Reductorul:
+% 1. Calculul elementelor geometrice ale angrenajului cilindric cu dințidrepți cunoscându-se:
+
+i = 13; %nr. ord
+n =(55+7*i)*10; %Turatie [rot/min]
+m = 3.5; %modulul [mm]
+z1 = 34;
+z2 = 42;
+
+x1 = 1;
+x2 = 1.16;
+% 2.Desen
+
+%% Mecanismul cu camă și tachet de translație:
+% 3.Să se  realizeze analiza cinematică  a mecanismului cu cama șitachet de translație 
+%(diagramele de variație ale spațiului s(𝜑), vitezei  reduse vω(φ) șiaccelerației reduse aω2(φ)) șiprofilul camei,cunoscându-se:
+
+h = 8+0.5*i; %[mm]
+phiu = 77+i;
+phiR = 80;
+phir = 68;  
+phiC = 122;
+%legea de miscare la urcare
+%legea de miscare la coborare
+alpha = 45; %
+
+%% Mecanismul pentru transmiterea intermitenta a miscarii:
+% 4.Elemente teoretice si de calcul. Cinematica mecanismului, Se cunosc: 
+
+na = 2; %numarul de antrenori
+z = 4;   %numarul de canale ale Crucii de Malta
+%L =         %distanta dintre centrele de rotație(O6 O7) [mm]
+
+
+
+% 5.Desen mecanism
+% 6.Diagrama de variatie a vitezei unghiularesi a acceleratiei unghiularela Crucea de Malta
